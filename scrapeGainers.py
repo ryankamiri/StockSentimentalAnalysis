09fa620dc5030561ad9e281b85ddc655fa309cd6
Ryan <ryanamiri05@gmail.com>
2022-06-29 23:32:44 -0700

    updated training data and models

--- scrapeGainers.py
+++ scrapeGainers.py
@@ -12,6 +12,10 @@ for article in finviz.getScreener("v=341&s=ta_topgainers", 20):
         #Write News
         data.append({"good_news": 1, "title": article["title"], "stock": article["ticker"], "date": str(article["date"]), "link": article["link"]})
 
+for article in finviz.getScreener("v=341&s=n_majornews&f=ta_perf_dup&ft=4", 3):
+    #Write News
+    data.append({"good_news": 1, "title": article["title"], "stock": article["ticker"], "date": str(article["date"]), "link": article["link"]})
+
 helper.writeData("gainers", data)
 
 print(f"Successfully scraped {len(data)} Top Gainer News Headlines!")
--- scrapeLosers.py
+++ scrapeLosers.py
@@ -1,11 +1,26 @@
 import helper
 import finviz
+from datetime import datetime
+from pytz import timezone
+
+MAX_DAYS = 3
 
 data = []
 
-for article in finviz.getScreener("v=341&s=ta_toplosers&f=cap_midover,sh_avgvol_o500,ta_perf_4wdown,ta_sma20_pb,ta_sma200_pb20&ft=3"):
+for article in finviz.getScreener("v=341&s=ta_toplosers&f=cap_midover,sh_avgvol_o500,ta_perf_4wdown,ta_sma20_pb,ta_sma200_pb20&ft=3", 2):
     #Write News
     data.append({"good_news": 0, "title": article["title"], "stock": article["ticker"], "date": str(article["date"]), "link": article["link"]})
+
+
+for article in finviz.getScreener("v=341&s=n_majornews&f=cap_midunder,ta_perf_ddown&ft=4", 3):
+    #Write News
+    data.append({"good_news": 0, "title": article["title"], "stock": article["ticker"], "date": str(article["date"]), "link": article["link"]})
+
+for article in finviz.getScreener("v=341&s=ta_newlow&f=sh_price_o5&ft=4", 5):
+    if (datetime.now(timezone('EST')) - article["date"]).days <= MAX_DAYS:
+        #Write News
+        data.append({"good_news": 0, "title": article["title"], "stock": article["ticker"], "date": str(article["date"]), "link": article["link"]})
+
 
 helper.writeData("losers", data)
 
